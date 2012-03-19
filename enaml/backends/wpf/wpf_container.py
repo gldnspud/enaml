a53a8a1f0f41bcf6cfad4e1a647b1e473033be68
itziakos <ioannist@enthought.com>
2012-03-19 04:52:49 +0000

    Remove Panel (e.i. canvas warping)
    It is not clean why the DesiredSize property needs to be overidden
    similar to what has been done for the other backends. More investigation
    is required to understand and implement the correct approach.

--- enaml/backends/wpf/wpf_container.py
+++ enaml/backends/wpf/wpf_container.py
@@ -10,58 +10,6 @@ from .wpf_constraints_widget import WPFConstraintsWidget
 from ...components.container import AbstractTkContainer
 
 
-class _Panel(Panel):
-    """ A subclass of Panel which calls back onto the shell
-    Container to get the size hint. If that size hint is invalid, it
-    falls back onto the WPF default.
-
-    """
-    def __init__(self, wpf_container, *args, **kwargs):
-        super(_Panel, self).__init__(*args, **kwargs)
-        self.wpf_container = weakref.ref(wpf_container)
-        print "Custom panel"
-
-    @property
-    def DesiredSize(self):
-        """ Computes the size hint from the given Container, falling
-        back on the default size hint computation if the Container
-        returns one that is invalid.
-
-        """
-        res = None
-        print "Overriden DesiredSize"
-        wpf_container = self.wpf_container()
-        if wpf_container is not None:
-            shell = wpf_container.shell_obj
-            if shell is not None:
-                sh = shell.size_hint()
-                print "container sizehint is {}".format(sh)
-                if sh != (-1, -1):
-                    res = sh
-        if res is None:
-            res = super(_Panel, self).DesiredSize
-        return res
-
-    # Because the Canvas panel does not report the width and high
-    # propery we have to override the normal behaviour and return the
-    # ActualWidth and Height instead
-    @property
-    def Width(self):
-        return self.ActualWidth
-
-    @Width.setter
-    def Width(self, value):
-        self.Width = value
-
-    @property
-    def Height(self):
-        return self.ActualHeight
-
-    @Height.setter
-    def Height(self, value):
-        self.Height = value
-
-
 class WPFContainer(WPFConstraintsWidget, AbstractTkContainer):
     """ A WPF implementation of Container.
 
@@ -71,7 +19,6 @@ class WPFContainer(WPFConstraintsWidget, AbstractTkContainer):
 
         """
         print "Create a container component"
-        self.widget = _Panel(parent)
         self.add_to_parent(parent)
 
     def initialize(self):
